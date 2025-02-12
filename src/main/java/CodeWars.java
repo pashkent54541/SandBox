178f5a6ecae01deb0592d293586c11051dacecdc
Pavel <user.name>
2025-02-12 12:32:13 +0300

    Ones and Zeros + tests

--- src/main/java/CodeWars.java
+++ src/main/java/CodeWars.java
@@ -1,42 +1,41 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Stream;
 
 public class CodeWars {
     /*
-Check to see if a string has the same amount of 'x's and 'o's.
-The method must return a boolean and be case insensitive. The string can contain any char.
-Examples input/output:
-XO("ooxx") => true
-XO("xooxx") => false
-XO("ooxXm") => true
-XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
-XO("zzoo") => false
+Given an array of ones and zeroes, convert the equivalent binary value to an integer.
+
+Eg: [0, 0, 0, 1] is treated as 0001 which is the binary representation of 1.
+
+Examples:
+
+Testing: [0, 0, 0, 1] ==> 1
+Testing: [0, 0, 1, 0] ==> 2
+Testing: [0, 1, 0, 1] ==> 5
+Testing: [1, 0, 0, 1] ==> 9
+Testing: [0, 0, 1, 0] ==> 2
+Testing: [0, 1, 1, 0] ==> 6
+Testing: [1, 1, 1, 1] ==> 15
+Testing: [1, 0, 1, 1] ==> 11
     */
     public static void main(String[] args) {
-
     }
 
-    public static boolean imperativeSolution(String str) {
-        int xCounter = 0;
-        int oCounter = 0;
-
-        for (int i = 0; i < str.length(); i++) {
-            if (str.toLowerCase().charAt(i) == 'x') {
-                xCounter++;
+    public static int imperativeSolution(List<Integer> binary) {
+        int powerOfTwo = 1;
+        int result = 0;
+        for (int i = binary.size() - 1; i >= 0; i--) {
+            if (binary.get(i) == 1) {
+                result += powerOfTwo;
             }
-            if (str.toLowerCase().charAt(i) == 'o') {
-                oCounter++;
-            }
+            powerOfTwo *= 2;
         }
-        return xCounter == oCounter;
+        return result;
     }
 
-    public static boolean declarativeSolution(String str) {
-        long xCount = str.toLowerCase().chars().filter(c -> c == 'o').count();
-        long oCount = str.toLowerCase().chars().filter(c -> c == 'x').count();
-
-        return xCount == oCount;
+    public static int declarativeSolution(List<Integer> binary) {
+        return binary.stream()
+                .reduce(0, (acc, bit) -> acc * 2 + bit);
     }
 }
--- src/test/java/SolutionTests.java
+++ src/test/java/SolutionTests.java
@@ -1,19 +1,27 @@
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SolutionTests extends UtilClass {
 
-    @ParameterizedTest
-    @MethodSource("data")
-    void imperativeSolutionTest(boolean expected, String input) {
-        assertEquals(expected,CodeWars.imperativeSolution(input));
+    @Test
+    void imperativeSolutionTest() {
+        assertEquals(1, CodeWars.imperativeSolution(new ArrayList<>(Arrays.asList(0,0,0,1))));
+        assertEquals(15, CodeWars.imperativeSolution(new ArrayList<>(Arrays.asList(1,1,1,1))));
+        assertEquals(6, CodeWars.imperativeSolution(new ArrayList<>(Arrays.asList(0,1,1,0))));
+        assertEquals(9, CodeWars.imperativeSolution(new ArrayList<>(Arrays.asList(1,0,0,1))));
     }
 
-    @ParameterizedTest
-    @MethodSource("data")
-    void declarativeSolutionTest(boolean expected, String input) {
-        assertEquals(expected,CodeWars.declarativeSolution(input));
+    @Test
+    void declarativeSolutionTest() {
+        assertEquals(1, CodeWars.declarativeSolution(new ArrayList<>(Arrays.asList(0,0,0,1))));
+        assertEquals(15, CodeWars.declarativeSolution(new ArrayList<>(Arrays.asList(1,1,1,1))));
+        assertEquals(6, CodeWars.declarativeSolution(new ArrayList<>(Arrays.asList(0,1,1,0))));
+        assertEquals(9, CodeWars.declarativeSolution(new ArrayList<>(Arrays.asList(1,0,0,1))));
     }
 }
