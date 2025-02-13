e14a741e61cb226f38e9765b8a3b67e6095187ed
Pavel <user.name>
2025-02-13 13:12:03 +0300

    Reverse words 1 failed test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,27 @@
-
 public class Main {
     public static void main(String[] args) {
+
+
     }
 
-    public static String fakeBinImp(String numberString) {
-       return numberString.replaceAll("[1-4]","0").replaceAll("[5-9]","1");
+    public static String reverseWords(final String s) {
+        String[] words = s.split(" ");
+        StringBuilder resultString = new StringBuilder();
+
+        for (int i = 0; i < words.length; i++) {
+            resultString.append(reverseWord(words[i]));
+            if (i < words.length - 1) {
+                resultString.append(" ");
+            }
+        }
+        return resultString.toString();
     }
 
-    public static String fakeBinDecl(String numberString) {
-        return numberString.chars().
-                mapToObj(c -> (char) c).
-                map(c -> (c >= '5') ? '1' : '0').
-                collect(StringBuilder::new, StringBuilder::append, StringBuilder::append).toString();
+    private static String reverseWord(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = s.length() - 1; i >= 0; i--) {
+            sb.append(s.charAt(i));
+        }
+        return sb.toString();
     }
 }
--- src/test/java/SolutionTests.java
+++ src/test/java/SolutionTests.java
@@ -6,15 +6,14 @@ public class SolutionTests extends UtilClass {
 
     @Test
     void imperativeSolutionTest() {
-        assertEquals("01011110001100111", Main.fakeBinImp("45385593107843568"));
-        assertEquals("101000111101101", Main.fakeBinImp("509321967506747"));
-        assertEquals("011011110000101010000011011", Main.fakeBinImp("366058562030849490134388085"));
+        assertEquals("ehT kciuq nworb xof spmuj revo eht yzal .god", Main.reverseWords("The quick brown fox jumps over the lazy dog."), "Input: \"The quick brown fox jumps over the lazy dog.\"");
+        assertEquals("elppa", Main.reverseWords("apple"), "Input: \"apple\"");
+        assertEquals("a b c d", Main.reverseWords("a b c d"), "Input: \"a b c d\"");
+        assertEquals("  elbuod  decaps  sdrow  ", Main.reverseWords("  double  spaced  words  "), "Input: \"  double  spaced  words  \"");
     }
 
     @Test
     void declarativeSolutionTest() {
-        assertEquals("01011110001100111", Main.fakeBinDecl("45385593107843568"));
-        assertEquals("101000111101101", Main.fakeBinDecl("509321967506747"));
-        assertEquals("011011110000101010000011011", Main.fakeBinDecl("366058562030849490134388085"));
+
     }
 }
